320dc2c3ec04139d21c79618080e156b59a70bb2
Ton3t <elton3t@gmail.com>
2026-07-26 10:04:52 +0200

    Add Git configuration

---
+++ scripts/packages.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+sudo apt update
+
+sudo apt install -y \
+git \
+curl \
+wget \
+eza \
+batcat \
+fzf \
+zoxide \
+ripgrep \
+fd-find \
+btop \
+fastfetch \
+jq \
+htop \
+tree \
+neovim
