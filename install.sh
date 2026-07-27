3c89d45e59919da1eb60af25055edf73d8fb0e87
Ton3t <elton3t@gmail.com>
2026-07-27 16:53:48 +0200

    feat: automate KDE themes installation

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ source "$ROOT_DIR/scripts/oh-my-zsh.sh"
 source "$ROOT_DIR/scripts/powerlevel10k.sh"
 source "$ROOT_DIR/scripts/zsh-plugins.sh"
 source "$ROOT_DIR/scripts/zsh.sh"
+source "$ROOT_DIR/scripts/themes.sh"
 
 main() {
 
@@ -24,6 +25,8 @@ main() {
     install_fonts
 
     install_zsh
+
+    install_themes
 
     create_symlinks
 
