356c2281983bf65779bfbc55a72041e45e9a1881
Ton3t <elton3t@gmail.com>
2026-07-28 12:13:23 +0200

    feat: finalize Powerlevel10k configuration

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -2,16 +2,15 @@
 
 create_symlinks() {
 
-    DOTFILES="$HOME/dotfiles/config"
+    local DOTFILES="$ROOT_DIR/config"
 
     info "Creando directorios..."
 
-    mkdir -p ~/.config/kitty
-    mkdir -p ~/.config/zsh
-    mkdir -p ~/.config/fastfetch
-    mkdir -p ~/.config/btop
-    mkdir -p ~/.config/btop/themes
-    mkdir -p ~/.config/nvim
+    mkdir -p "$HOME/.config/kitty"
+    mkdir -p "$HOME/.config/zsh"
+    mkdir -p "$HOME/.config/fastfetch"
+    mkdir -p "$HOME/.config/btop/themes"
+    mkdir -p "$HOME/.config/nvim"
 
     # Tema de btop
     link_file \
@@ -23,6 +22,7 @@ create_symlinks() {
         "kitty/kitty.conf:.config/kitty/kitty.conf"
         "git/.gitconfig:.gitconfig"
         "zsh/.zshrc:.zshrc"
+        "zsh/.p10k.zsh:.p10k.zsh"
         "zsh/aliases.zsh:.config/zsh/aliases.zsh"
         "zsh/history.zsh:.config/zsh/history.zsh"
         "zsh/plugins.zsh:.config/zsh/plugins.zsh"
@@ -51,4 +51,5 @@ create_symlinks() {
             link_file "$DOTFILES/$src" "$HOME/$dest"
         done
     fi
+
 }
